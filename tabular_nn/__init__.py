6644e2a174b11bedc5fc5f2a76afe1241dad7e39
TQCAI <1468632627@qq.com>
2020-12-16 14:32:13 +0800

    initial commit

---
+++ tabular_nn/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : qichun tang
+# @Contact    : tqichun@gmail.com
+from tabular_nn.component.equidistance import EquidistanceEncoder
+from tabular_nn.component.embedding_encoder import EmbeddingEncoder
